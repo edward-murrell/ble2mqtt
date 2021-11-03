b1f154c5e3575da9b930b6262db47a8e0493b808
Edward Murrell <edward@murrell.co.nz>
2021-11-03 17:46:19 +1100

    Rewrite to support configurable mqtt endpoint & discoverable adapter name.

--- env_sensor.go
+++ env_sensor.go
@@ -54,7 +54,7 @@ func (b *EnvSensor) GetTemperature() (value float32) {
 		fmt.Printf("error %s", err)
 		return 0
 	}
-	raw := (uint16(c[1]) * 2560) + uint16(c[0])
+	raw := (uint16(c[1]) * 256) + uint16(c[0])
 	value = float32(raw) / 10
 	return
 }
--- start.go
+++ start.go
@@ -6,37 +6,46 @@ import (
 	"os"
 	"time"
 
+	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/platforms/ble"
 	"gobot.io/x/gobot/platforms/mqtt"
-	"gobot.io/x/gobot"
 )
 
 
-
 func main() {
-	bleAdaptor := ble.NewClientAdaptor(os.Args[1])
+	mqttUrl := os.Args[1]
+	mqttAdaptor := mqtt.NewAdaptor(mqttUrl, "ble2mqtt")
+	mqttError := mqttAdaptor.Connect()
+
+	fmt.Printf("Starting connection to MQTT server: %s... ", mqttUrl)
+	if mqttError != nil {
+		fmt.Printf("failed - %s\n", mqttError)
+		os.Exit(1)
+	} else {
+		fmt.Printf("success.\n")
+	}
+
+	bleAdaptor := ble.NewClientAdaptor(os.Args[2])
 	envAdapt := NewEnvironmentSensor(bleAdaptor)
-	mqttAdaptor := mqtt.NewAdaptor("tcp://192.168.183.123:1883", "ble2mqtt")
-	mqttAdaptor.Connect()
-	mqttDriver := mqtt.NewDriver(mqttAdaptor, "home-assistant/sensor/ABC/state")
+	envAdapt.Connection().Name()
 
 	work := func() {
 		gobot.Every(5*time.Second, func() {
+			fmt.Printf("Querying %s\n", envAdapt.Connection().Name())
 			packet := envAdapt.GetPacket()
 
 			fmt.Printf("Temperature level: %.1f C\n", packet.Temperature)
 			fmt.Printf("Humidity level: %.1f%%\n", packet.Humidity)
 
-//			var jsonBytes []byte
 			jsonBytes, err := json.Marshal(packet)
 			if err != nil {
-				fmt.Printf("Error: %s", err)
+				fmt.Printf("error marshalling packet: %s", err)
+				return
 			}
-			fmt.Printf("Send %s\n", string(jsonBytes))
+			topic := fmt.Sprintf("sensor/%s/state", envAdapt.Connection().Name())
+			fmt.Printf("Publishing to topic %s: %s\n", topic, string(jsonBytes))
 
-
-
-			mqttDriver.Publish(jsonBytes)
+			mqttAdaptor.Publish("asd", jsonBytes)
 		})
 	}
 
@@ -48,4 +57,3 @@ func main() {
 
 	robot.Start()
 }
-
