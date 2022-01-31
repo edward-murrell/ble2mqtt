47e57e2495bf54e4dd5d470c6566f02b65855f88
Edward Murrell <edward@murrell.co.nz>
2022-01-31 18:33:50 +1100

    Refactor the app loop to make it easier to test and de-couple loop from BLE interface.

--- loop.go
+++ loop.go
@@ -7,29 +7,42 @@ import (
 	"tinygo.org/x/bluetooth"
 )
 
+type appLoop struct {
+	config *Config
+	sensors map[bluetooth.MAC]AtcSensor
+	mqttAdaptor *mqtt.Adaptor
+}
+
 func startListening(adapter *bluetooth.Adapter, sensors map[bluetooth.MAC]AtcSensor, config *Config, mqttAdaptor *mqtt.Adaptor) {
-	// Enable BLE interface.
-	err := adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
-		for mac, sensor := range sensors {
-			if device.Address.String() != mac.String() {
+	loop := &appLoop{
+		config:      config,
+		sensors:     sensors,
+		mqttAdaptor: mqttAdaptor,
+	}
+
+	err := adapter.Scan(loop.handlePacket)
+	panicCheck("scan error", err)
+}
+
+func (loop *appLoop) handlePacket(adapter *bluetooth.Adapter, blePacket bluetooth.ScanResult) {
+	for mac, sensor := range loop.sensors {
+		if blePacket.Address.String() != mac.String() {
+			continue
+		}
+		change, failure := sensor.UpdateDevice(blePacket.AdvertisementPayload)
+		if failure != nil {
+			println(failure.Error())
+		}
+		if change {
+			jsonBytes, err := json.Marshal(sensor.Packet())
+			if err != nil {
+				fmt.Printf("error marshalling packet: %s", err)
 				continue
 			}
-			change, failure := sensor.UpdateDevice(device.AdvertisementPayload)
-			if failure != nil {
-				println(failure.Error())
-			}
-			if change {
-				jsonBytes, err := json.Marshal(sensor.Packet())
-				if err != nil {
-					fmt.Printf("error marshalling packet: %s", err)
-					continue
-				}
 
-				topic := fmt.Sprintf(config.MQTT.Path, sensor.Name()) // TODO: Move into sensor?
-				fmt.Printf("Publishing to topic %s: %s\n", topic, string(jsonBytes))
-				mqttAdaptor.Publish(topic, jsonBytes)
-			}
+			topic := fmt.Sprintf(loop.config.MQTT.Path, sensor.Name()) // TODO: Move into sensor?
+			fmt.Printf("Publishing to topic %s: %s\n", topic, string(jsonBytes))
+			loop.mqttAdaptor.Publish(topic, jsonBytes)
 		}
-	})
-	panicCheck("scan error", err)
+	}
 }
