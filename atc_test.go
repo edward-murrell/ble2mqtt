3d2ee56b738fa11e3b59590f8be26f755672e7bd
Edward Murrell <edward@murrell.co.nz>
2022-01-31 18:14:28 +1100

    Add test for checking if same packet causes the sensor to not update twice.

--- atc_test.go
+++ atc_test.go
@@ -11,7 +11,7 @@ func TestAtcSensor(t *testing.T) {
 	mac, _ := bluetooth.ParseMAC("AA:BB:CC:11:22:34")
 
 	t.Run("Test Name updated", func(t *testing.T) {
-		update := fakeScanResult{
+		update := &fakeScanResult{
 			Address:      bluetooth.MACAddress{},
 			RSSI:         50,
 			localname:    "NEW_NAME",
@@ -22,12 +22,62 @@ func TestAtcSensor(t *testing.T) {
 		}
 		sensor := NewATCSensor(mac)
 
-		response, err := sensor.UpdateDevice(&update)
+		response, err := sensor.UpdateDevice(update)
 		actual := sensor.Name()
 
 		assert.Equal(t, true, response)
 		assert.Equal(t, "NEW_NAME", actual)
 		assert.Nil(t, err)
+	})
+
+	t.Run("Test data output", func(t *testing.T) {
+		update := &fakeScanResult{
+			Address:      bluetooth.MACAddress{},
+			RSSI:         50,
+			localname:    "NEW_NAME",
+			ServiceUUIDs: []bluetooth.UUID{},
+			ServiceData:  map[string][]byte{
+				"0000181a-0000-1000-8000-00805f9b34fb": {0xaa, 0xbb, 0xcc, 0x11, 0x22, 0x34, 0x01, 0x14, 0x29, 0x59, 0x0b, 0xc2, 0x13},
+			},
+		}
+		expected := AtcPacket{
+			Temperature: 27.6,
+			Humidity:    41,
+			Battery:     89,
+		}
+		sensor := NewATCSensor(mac)
+		sensor.UpdateDevice(update)
+
+		actual := sensor.Packet()
+
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("Test data output", func(t *testing.T) {
+		update := &fakeScanResult{
+			Address:      bluetooth.MACAddress{},
+			RSSI:         50,
+			localname:    "NEW_NAME",
+			ServiceUUIDs: []bluetooth.UUID{},
+			ServiceData:  map[string][]byte{
+				"0000181a-0000-1000-8000-00805f9b34fb": {0xaa, 0xbb, 0xcc, 0x11, 0x22, 0x34, 0x01, 0x14, 0x29, 0x59, 0x0b, 0xc2, 0x13},
+			},
+		}
+		expectedPacket := AtcPacket{
+			Temperature: 27.6,
+			Humidity:    41,
+			Battery:     89,
+		}
+		sensor := NewATCSensor(mac)
+		response1, err1 := sensor.UpdateDevice(update)
+		response2, err2 := sensor.UpdateDevice(update)
+
+		actual := sensor.Packet()
+		assert.Equal(t, true, response1)
+		assert.Equal(t, false, response2)
+		assert.Nil(t, err1)
+		assert.Nil(t, err2)
+		assert.Equal(t, expectedPacket, actual)
 	})
 }
 
